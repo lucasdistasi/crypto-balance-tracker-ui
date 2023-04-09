363c2f2fb94e1fdde16b28bfe7483b4b744f51c2
Di Stasi Lucas <distasilucas@gmail.com>
2023-04-09 16:57:19 -0300

    CBTUI-001 - Add TailwindCSS and some basic components

---
+++ src/components/buttons/DeleteButton.tsx
@@ -0,0 +1,11 @@
+const DeleteButton = () => {
+
+  return (
+    <button type="button"
+            className="sm:ml-0 lg:ml-1 w-32 text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
+      Delete
+    </button>
+  );
+}
+
+export default DeleteButton
