394ba0d64697aefc1f3eb2faf3c97506de20810c
Di Stasi Lucas <distasilucas@gmail.com>
2023-04-29 12:01:35 -0300

    CBTUI-016 - Add custom hook to retrieve cryptos platforms balances

--- src/components/dashboard/CryptosBalancesPlatformsTable.tsx
+++ src/components/dashboard/CryptosBalancesPlatformsTable.tsx
@@ -1,27 +1,8 @@
-import {useEffect, useState} from "react";
-import {CryptosBalancesPlatformsResponse} from "../../response/CryptosBalancesPlatformsResponse";
-import {HTTP_METHOD} from "../../model/HttpMethod";
-import {DASHBOARDS_CRYPTOS_PLATFORMS_BALANCES_ENDPOINT} from "../../constants/Constants";
+import {useCryptosPlatformsBalances} from "../../hooks/useCryptosPlatformsBalances";
 
 const CryptosBalancesPlatformsTable = () => {
 
-  const [response, setResponse] = useState<CryptosBalancesPlatformsResponse>({
-    coinInfoResponse: [],
-  });
-
-  // TODO - custom hooke to avoid duplicate code
-  useEffect(() => {
-    fetch(DASHBOARDS_CRYPTOS_PLATFORMS_BALANCES_ENDPOINT, {
-      method: HTTP_METHOD.GET
-    }).then(response => {
-      if (response.status === 200) {
-        const apiResponse = response.json();
-        apiResponse.then(responseBody => {
-          setResponse(responseBody);
-        });
-      }
-    });
-  }, []);
+  const {response} = useCryptosPlatformsBalances();
 
   return (
     response?.coinInfoResponse?.length > 0 ?
