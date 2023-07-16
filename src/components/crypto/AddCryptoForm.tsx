7b45a81bcd0d830530e6e413abdd92df5502cd61
Di Stasi Lucas <distasilucas@gmail.com>
2023-07-16 12:03:18 -0300

    CBTUI-027 - Use Formik and Yup for add/edit crypto

---
+++ src/components/crypto/AddCryptoForm.tsx
@@ -0,0 +1,83 @@
+import {useNavigate} from "react-router-dom";
+import {Form, Formik} from "formik";
+import {CRYPTOS_ENDPOINT} from "../../constants/Constants";
+import axios from "axios";
+import CryptoPlatformDropdown from "../form/CryptoPlatformDropdown";
+import EditableTextInput from "../form/EditableTextInput";
+import React, {useState} from "react";
+import SubmitButton from "../form/SubmitButton";
+import ErrorResponse from "../../model/response/ErrorResponse";
+import ErrorListAlert from "../page/ErrorListAlert";
+import {addCryptoValidationSchema} from "../../constants/ValidationSchemas";
+
+const AddCryptoForm = () => {
+
+  const navigate = useNavigate();
+
+  const [apiErrors, setApiErrors] = useState<ErrorResponse[]>([]);
+
+  const addCrypto = async ({...values}) => {
+    const {cryptoName, quantity, platform} = values;
+
+    try {
+      await axios.post(CRYPTOS_ENDPOINT, {
+        coinName: cryptoName,
+        quantity,
+        platform,
+      });
+
+      navigate("/cryptos");
+    } catch (error: any) {
+      const {status} = error.response;
+      if (status >= 400 && status < 500) {
+        setApiErrors(error.response.data.errors);
+      }
+
+      if (status >= 500) {
+        navigate("/error");
+      }
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center min-h-screen">
+      <h1 className="text-4xl text-gray-900 text-center my-10">
+        Add Crypto
+      </h1>
+
+      {
+        apiErrors && apiErrors.length >= 1 &&
+        <ErrorListAlert
+          title="Error adding crypto"
+          errors={apiErrors}/>
+      }
+
+      <Formik
+        initialValues={{
+          cryptoName: '',
+          quantity: 0,
+          platform: ''
+        }}
+        validationSchema={addCryptoValidationSchema}
+        onSubmit={(values, {setSubmitting}) => {
+          addCrypto(values);
+        }}>
+
+        <Form className="my-4 w-10/12 md:w-9/12 lg:w-1/2">
+          <EditableTextInput label="Crypto Name"
+                             type="text"
+                             name="cryptoName"
+                             placeholder="Bitcoin"/>
+          <EditableTextInput label="Quantity"
+                             type="number"
+                             name="quantity"/>
+          <CryptoPlatformDropdown label="Platform"
+                                  name="platform"/>
+          <SubmitButton text="Add crypto"/>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default AddCryptoForm
--- src/pages/crypto/EditCryptoPage.tsx
+++ src/pages/crypto/EditCryptoPage.tsx
@@ -2,15 +2,14 @@ import Navbar from "../../components/page/Navbar";
 import Footer from "../../components/page/Footer";
 import withScrollToTop from "../../hoc/withScrollToTop";
 import React, {Fragment} from "react";
-import CryptoForm from "../../components/crypto/CryptoForm";
-import {FORM_ACTION} from "../../model/FormAction";
+import EditCryptoForm from "../../components/crypto/EditCryptoForm";
 
 const EditCryptoPage = () => {
 
   return (
     <Fragment>
       <Navbar/>
-      <CryptoForm action={FORM_ACTION.UPDATE}/>
+      <EditCryptoForm/>
       <Footer/>
     </Fragment>
   );
