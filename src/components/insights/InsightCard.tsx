1424b5acefe14a09c273189e3cf0c939dca6bf4c
Di Stasi Lucas <distasilucas@gmail.com>
2025-04-23 09:16:05 -0300

    CBTUI-67 - Add use-count-up library

--- src/components/insights/InsightCard.tsx
+++ src/components/insights/InsightCard.tsx
@@ -1,3 +1,5 @@
+import {CountUp} from "use-count-up";
+
 const InsightCard = ({title, value, decimals, symbol}: {
   title: string,
   value: number,
@@ -6,16 +8,24 @@ const InsightCard = ({title, value, decimals, symbol}: {
 }) => {
 
   return (
-    <div className="mb-8 p-6 bg-gray-100 border border-gray-200 rounded-lg shadow text-left md:w-full xl:w-1/6 dark:bg-dark-1 dark:border-gray-500">
+    <div
+      className="mb-8 p-6 bg-gray-100 border border-gray-200 rounded-lg shadow text-left md:w-full xl:w-1/6 dark:bg-dark-1 dark:border-gray-500">
       <h5 className="w-full mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
         {
           title
         }
       </h5>
-      <div className="font-semibold text-lg text-gray-700 dark:text-gray-400">
-        {
-          `${symbol} ${value.toLocaleString('en-US', { minimumFractionDigits: decimals, maximumFractionDigits: decimals })}`
-        }
+      <div className="font-semibold text-lg text-gray-700 dark:text-gray-300">
+        {`${symbol} `}
+        <CountUp
+          isCounting
+          end={value}
+          duration={1.25}
+          formatter={(value) => value.toLocaleString('en-US', {
+            minimumFractionDigits: decimals,
+            maximumFractionDigits: decimals,
+          })}
+        />
       </div>
     </div>
   );
